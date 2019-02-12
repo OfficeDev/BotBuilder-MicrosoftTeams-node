ce44d9b8b34a337d1029ce913162362d3138a53c
Robin Liao <yilia@microsoft.com>
2019-02-12 15:10:09 -0800

    fix dependency

--- botbuilder-teams-js/src/schema/operations/teams.ts
+++ botbuilder-teams-js/src/schema/operations/teams.ts
@@ -4,7 +4,7 @@
  * regenerated.
  */
 
-import * as msRest from 'ms-rest-js';
+import * as msRest from 'botframework-connector/node_modules/ms-rest-js';
 import * as Models from '../models';
 import * as Mappers from '../models/teamsMappers';
 import * as Parameters from '../models/parameters';
